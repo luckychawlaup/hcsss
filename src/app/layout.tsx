2fbf5340a4632bd045c5eb7b04045c0a87ac6250
Lucky Chawla <luckychawlaup@gmail.com>
2025-09-11 19:40:05 +0000

    Make the entire website theme auto adaptive to browser theme light or da

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} light`}>
+    <html lang="en" className={`${poppins.variable}`} suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
