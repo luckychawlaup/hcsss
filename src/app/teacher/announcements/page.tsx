87e7fed25669134d410046dca93dd11ff764024e
Lucky Chawla <luckychawlaup@gmail.com>
2025-09-28 03:46:21 +0000

    the send annoucemenet option i still not appearing in mobile screns it s

--- src/app/teacher/announcements/page.tsx
+++ src/app/teacher/announcements/page.tsx
@@ -133,11 +133,11 @@ export default function TeacherAnnouncementsPage() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-background">
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col overflow-hidden">
         <Header title="Announcements" showAvatar={true} />
-        <main className="flex flex-1">
+        <main className="flex flex-1 overflow-hidden">
           {isMobile ? (
-            <div className="w-full flex flex-col">
+            <div className="w-full flex flex-col h-full">
               {!selectedClass ? (
                  <ClassChatGroup 
                     assignedClasses={assignedClasses}
