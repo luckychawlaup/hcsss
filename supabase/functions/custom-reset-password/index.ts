db39a385cf7c543cdd4b73d9198239e8a2d368cb
Lucky Chawla <luckychawlaup@gmail.com>
2025-09-29 17:46:21 +0000

    "worker boot error: Uncaught SyntaxError: The requested module 'https://

--- supabase/functions/custom-reset-password/index.ts
+++ supabase/functions/custom-reset-password/index.ts
@@ -1,5 +1,4 @@
 
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.0.0";
 import { v4 } from "https://deno.land/std@0.168.0/uuid/mod.ts";
